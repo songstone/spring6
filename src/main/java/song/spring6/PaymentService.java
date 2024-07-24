b9402b8c459f94c9c02ce4384cae1855f8d2862c
songstone <thdtmdgus5505@gmail.com>
2024-07-24 10:11:26 +0900

    feat: payment 속성, 기본 생성자, getter 정의

--- src/main/java/song/spring6/PaymentService.java
+++ src/main/java/song/spring6/PaymentService.java
@@ -1,14 +1,21 @@
 package song.spring6;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class PaymentService {
-    public Payment prepare() {
-        return new Payment();
+    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) {
+        // TODO 환율 가져오기
+        // TODO 금액 계산
+        // TODO 시간 계산
+        return new Payment(orderId, currency, foreignCurrencyAmount, BigDecimal.ZERO, BigDecimal.ZERO,
+            LocalDateTime.now());
     }
 
 
     public static void main(String[] args) {
         PaymentService paymentService = new PaymentService();
-        Payment payment = paymentService.prepare();
+        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
     }
 }
