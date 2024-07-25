c09cf43d008def29d45cb99a2096c891a8821a58
songstone <thdtmdgus5505@gmail.com>
2024-07-25 09:57:23 +0900

    refactor: 인터페이스 도입

---
+++ src/main/java/song/spring6/ExRateProvider.java
@@ -0,0 +1,8 @@
+package song.spring6;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+public interface ExRateProvider {
+    BigDecimal getExRate(String currency) throws IOException;
+}
--- src/main/java/song/spring6/SimpleExRateProvider.java
+++ src/main/java/song/spring6/SimpleExRateProvider.java
@@ -3,9 +3,10 @@ package song.spring6;
 import java.io.IOException;
 import java.math.BigDecimal;
 
-public class SimpleExRatePaymentService extends PaymentService{
+public class SimpleExRateProvider implements ExRateProvider {
+
     @Override
-    BigDecimal getExRate(String currency) throws IOException {
+    public BigDecimal getExRate(String currency) throws IOException {
         if(currency.equals("USD")) return BigDecimal.valueOf(1000);
 
         throw new IllegalArgumentException("지원되지 않는 통화 입니다.");
