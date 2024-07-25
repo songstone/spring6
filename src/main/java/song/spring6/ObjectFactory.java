7318ce21ecc59ee8226e572cb73c2f42d81bc87d
songstone <thdtmdgus5505@gmail.com>
2024-07-25 10:10:04 +0900

    refactor: 오브젝트 팩토리 도입

---
+++ src/main/java/song/spring6/ObjectFactory.java
@@ -0,0 +1,11 @@
+package song.spring6;
+
+public class ObjectFactory {
+    public PaymentService paymentService() {
+        return new PaymentService(exRateProvider());
+    }
+
+    public ExRateProvider exRateProvider() {
+        return new WebApiExRateProvider();
+    }
+}
