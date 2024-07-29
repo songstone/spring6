e9e07cf55c7cf12a5772d02bc408257f9d6f774a
songstone <thdtmdgus5505@gmail.com>
2024-07-29 10:50:06 +0900

    refactor: 컴포넌트 스캔 도입

--- src/main/java/song/spring6/ObjectFactory.java
+++ src/main/java/song/spring6/ObjectFactory.java
@@ -1,17 +1,10 @@
 package song.spring6;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan
 public class ObjectFactory {
-    @Bean
-    public PaymentService paymentService() {
-        return new PaymentService(exRateProvider());
-    }
 
-    @Bean
-    public ExRateProvider exRateProvider() {
-        return new SimpleExRateProvider();
-    }
 }
