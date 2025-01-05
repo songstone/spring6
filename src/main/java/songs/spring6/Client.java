620b6059ee7b74deaf7c23459d27c2bd4b6edc05
songstone <thdtmdgus5505@gmail.com>
2025-01-06 05:22:15 +0900

    feat: 빈 팩토리 도입

--- src/main/java/songs/spring6/Client.java
+++ src/main/java/songs/spring6/Client.java
@@ -1,11 +1,15 @@
 package songs.spring6;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import java.math.BigDecimal;
 
 public class Client {
     public static void main(String[] args) throws Exception {
-        ObjectFactory objectFactory = new ObjectFactory();
-        PaymentService paymentService = objectFactory.paymentService();
+        BeanFactory beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
+        PaymentService paymentService = beanFactory.getBean(PaymentService.class);
+
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
     }
--- src/main/java/songs/spring6/ObjectFactory.java
+++ src/main/java/songs/spring6/ObjectFactory.java
@@ -1,10 +1,16 @@
 package songs.spring6;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ObjectFactory {
+    @Bean
     public PaymentService paymentService() {
         return new PaymentService(exRateProvider());
     }
 
+    @Bean
     public ExRateProvider exRateProvider() {
         return new WebApiExRateProvider();
     }
