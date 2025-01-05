6287c6ecefc59a6684bac2af547cfafe6a68f477
songstone <thdtmdgus5505@gmail.com>
2025-01-06 05:14:00 +0900

    feat: 오브젝트 팩토리 도입

--- src/main/java/songs/spring6/Client.java
+++ src/main/java/songs/spring6/Client.java
@@ -1,11 +1,11 @@
 package songs.spring6;
 
-import java.io.IOException;
 import java.math.BigDecimal;
 
 public class Client {
     public static void main(String[] args) throws Exception {
-        PaymentService paymentService = new PaymentService(new WebApiExRateProvider());
+        ObjectFactory objectFactory = new ObjectFactory();
+        PaymentService paymentService = objectFactory.paymentService();
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
     }
