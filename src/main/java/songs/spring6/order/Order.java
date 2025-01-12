bb215d45086d9e4ebe2969f92a064ffb1f3324d1
songstone <thdtmdgus5505@gmail.com>
2025-01-12 22:31:46 +0900

    refactor: xml로 jpa 메타 정보 분리

--- src/main/java/songs/spring6/order/Order.java
+++ src/main/java/songs/spring6/order/Order.java
@@ -1,21 +1,16 @@
 package songs.spring6.order;
 
-import jakarta.persistence.*;
-
 import java.math.BigDecimal;
 
-@Entity
-@Table(name = "orders")
 public class Order {
-    @Id @GeneratedValue
     private Long id;
 
-    @Column(unique = true)
     private String no;
 
     private BigDecimal total;
 
-    public Order() {}
+    public Order() {
+    }
 
     public Order(String no, BigDecimal total) {
         this.no = no;
